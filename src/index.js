ba590cc8f35e0415b2cbfa377c6e3eb5178bef61
maimuna <mohamudmaimuna003@gmail.com>
2023-06-29 12:03:48 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let addTask = document.querySelector("#create-task-form");
+console.log(addTask);
+addTask . addEventListener("submmit",(e) =>{
+  e.preventDefault();
+  onsubmit(e.target.new_task.value);
 });
+});
+function onsubmit(todo){
+  let p = document.createElement("p");
+  console.log(p);
+  p.textContent = `${todo}`;
+  let btn = document.createElement("button");
+  btn.textContent = "Delete Task";
+  btn .addEventListener("click , deleteTasks");
+  p.appendChild (btn);
+  console.log(btn);
+  document.querySelector("#tasks").appendChild(p);
+
+}
+
+  
+function deleteTasks(e){
+  e.target.parentNode.remove();
+}
